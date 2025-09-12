4a6bb57fe0f1e0391660878fa09ac1fe19ff4136
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-09-12 20:48:05 +1200

    Add files via upload

---
+++ try.py
@@ -0,0 +1,6 @@
+try:
+    num = int(input("enter ur lottery number: "))
+    print("ok ur numbers are ", num, "get scammed lol womp womp")
+except ValueError as ex:
+    print("i said number and u put this?! this an", ex, "you know!?")
+print("i am outside ur house btw")
