154143f43154c9a29693b92907546930d091e0a1
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-09-12 21:15:25 +1200

    Add files via upload

---
+++ bye bye.py
@@ -0,0 +1,11 @@
+valid = False
+while not valid:
+    try:
+        n = int(input("enter a number: "))
+        while n%2 == 0:
+            print("OK THANKS U FINALLY PUT SOMETHING CORRECTLY WE WERE CHECKING TO IF IT WAS A EVEN NUMBER SINCE YOU DID IT CORRECTLY U WIN 1000000000000000.999999999999999.67 DOLLARS (OR SO U THOUGHT HA WOMP WOMP)")
+            valid = True
+            break
+    except ValueError:
+        print("its invalid can u humans just give a break i work 9/5 asking u guys for number and u still give letters like how dumb are u can you just let me do my job correctly im one away from being fired just one correct nu-mber and i will finally be able to retire thanks")
+    
